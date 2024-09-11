85ca798340510155607382ae8252701920b9f02b
Gaurav Saxena <99750798+Saxena-G@users.noreply.github.com>
2024-09-11 19:49:08 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,40 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int Id;
+    private String Name;
+    private String Address;
+    private int age;
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
